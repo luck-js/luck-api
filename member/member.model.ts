e377a9e46d5456f74fa0653b613fb363c861e3e8
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-04 09:37:44 +0200

    initialized member repository

---
+++ member/member.model.ts
@@ -0,0 +1,5 @@
+export interface IMember {
+    name: string,
+    uniqueLink: string,
+    matchedMemberId: string
+}
---
+++ member/member.repository.ts
@@ -0,0 +1,11 @@
+import {IMember} from './member.model';
+
+export class MemberRepository {
+    public add(id: string, name: string): IMember {
+        return null
+    }
+
+    public getList(): IMember[] {
+        return null
+    }
+}
