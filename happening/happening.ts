e2a45b0a9a307425c5286e095dada12de6131900
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-04 09:05:42 +0200

    import Happening.class

---
+++ happening/happening.ts
@@ -0,0 +1,13 @@
+import {IHappening} from './happening.model';
+
+export class Happening implements IHappening {
+    public isPublish = false;
+
+    public addMember(): any {
+
+    }
+
+    public getMembers(): any {
+
+    }
+}
