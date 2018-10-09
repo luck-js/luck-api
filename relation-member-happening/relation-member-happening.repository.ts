d55c2165d5295362bd9ffecce60a013581ab52d8
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-09 10:02:47 +0200

    implement correctly find method for relation-member-happening.repository

--- relation-member-happening/relation-member-happening.repository.ts
+++ relation-member-happening/relation-member-happening.repository.ts
@@ -2,8 +2,16 @@ import {IRelationMemberHappening} from './relation-member-happening.model';
 
 
 export class RelationMemberHappeningRepository {
+    list: IRelationMemberHappening[] = [];
 
     public add(relationMemberHappening: IRelationMemberHappening): IRelationMemberHappening {
+        this.list.push(relationMemberHappening);
+
         return relationMemberHappening
+    }
+
+    public get(id: string): IRelationMemberHappening {
+        // @ts-ignore
+        return this.list.find((el) => el.id === id)
     }
 }
