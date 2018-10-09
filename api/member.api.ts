14b5832526a287611a25233a400a107c86d94740
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-09 11:16:41 +0200

    implement correctly member.api getMatchedMember

--- api/member.api.ts
+++ api/member.api.ts
@@ -6,4 +6,8 @@ export class MemberApi {
     public getMemberInformationView(id: string){
         return this.memberService.getMemberInformationView(id);
     }
+
+    public getMatchedMember(id: string){
+        return this.memberService.getMatchedMember(id);
+    }
 }
