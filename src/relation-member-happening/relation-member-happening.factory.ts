78928a19d9ac4cd06054b8cbb057260b9cdc4427
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-19 09:50:49 +0200

    implemented method to generate uuid for relation-member-happening.factory

--- src/relation-member-happening/relation-member-happening.factory.ts
+++ src/relation-member-happening/relation-member-happening.factory.ts
@@ -2,12 +2,18 @@ import { inject, injectable } from 'inversify';
 import IDENTIFIER from '../identifiers';
 import { IRelationMemberHappening } from './relation-member-happening.model';
 import { RelationMemberHappening } from './relation-member-happening';
+import { UuidGenerationService } from '../member/uuid-generation.service';
 
 @injectable()
 export class RelationMemberHappeningFactory {
     constructor(
+        private uuidGenerationService: UuidGenerationService,
         @inject(IDENTIFIER.DIFactoryRelationMemberHappening)
         private DIFactoryRelationMemberHappening: (option: IRelationMemberHappening) => RelationMemberHappening) {
+    }
+
+    public generateUuid(): string {
+        return this.uuidGenerationService.createNewUuid();
     }
 
     public create(option: IRelationMemberHappening): RelationMemberHappening {
