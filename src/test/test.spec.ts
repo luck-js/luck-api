f81ea274260932e47cada2eedda8622ebb6b8b9b
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-19 09:54:53 +0200

    refactored process to creating new happening by test.spec.ts

--- src/test/test.spec.ts
+++ src/test/test.spec.ts
@@ -1,15 +1,16 @@
 import 'mocha';
 import { Container } from 'inversify';
 import { Happening } from '../happening/happening';
-import { HappeningFactory } from '../happening/happening.factory';
 import { DIContainerProvider } from '../di-container';
 import IDENTIFIER from '../identifiers';
+import { RelationMemberHappeningService } from '../relation-member-happening/relation-member-happening.service';
 
 export const initialDependencies = (MEMBER_INITIAL_LIST_MOCK?, HAPPENING_INITIAL_LIST_MOCK?): Container => {
     return DIContainerProvider(MEMBER_INITIAL_LIST_MOCK, HAPPENING_INITIAL_LIST_MOCK);
 };
 
 export function createHappening(DIContainer: Container, { name, description }: any): Happening {
-    const happeningFactory = DIContainer.get<HappeningFactory>(IDENTIFIER.HappeningFactory);
-    return happeningFactory.create({ name, description });
+    const relationMemberHappeningService = DIContainer.get<RelationMemberHappeningService>(IDENTIFIER.RelationMemberHappeningService);
+    const relationId = relationMemberHappeningService.createHappening();
+    return relationMemberHappeningService.editHappening(relationId, { name, description });
 }
