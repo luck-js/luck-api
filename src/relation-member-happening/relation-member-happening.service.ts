de19681b6aa3352ee67430b560126315f74a9484
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-19 09:48:16 +0200

    implemented process logic to creating new happening for relation-member-happening.service

--- src/relation-member-happening/relation-member-happening.service.ts
+++ src/relation-member-happening/relation-member-happening.service.ts
@@ -2,11 +2,32 @@ import { injectable } from 'inversify';
 import { RelationMemberHappeningRepository } from './relation-member-happening.repository';
 import { IParticipationHappeningView } from './participation-happening-view.model';
 import { IMemberView } from './member-view.model';
+import { HappeningFactory } from '../happening/happening.factory';
+import { RelationMemberHappeningFactory } from './relation-member-happening.factory';
+import { HappeningRepository } from '../happening/happening.repository';
 
 @injectable()
 export class RelationMemberHappeningService {
     constructor(
-        private relationMemberHappeningRepository: RelationMemberHappeningRepository) {
+        private relationMemberHappeningRepository: RelationMemberHappeningRepository,
+        private happeningRepository: HappeningRepository,
+        private happeningFactory: HappeningFactory,
+        private relationMemberHappeningFactory: RelationMemberHappeningFactory) {
+    }
+
+    public createHappening(): string {
+        const relationId = this.relationMemberHappeningFactory.generateUuid();
+
+        const happening = this.happeningFactory.create();
+        const member = happening.addMember(relationId);
+
+        const relation = this.relationMemberHappeningFactory.create(relationId, happening, member);
+
+        this.happeningRepository.add(happening);
+
+        this.relationMemberHappeningRepository.add({ id: relation.Id, happeningId: happening.id, memberId: member.id });
+
+        return relation.Id;
     }
 
     public getDataView(id: string): IParticipationHappeningView {
