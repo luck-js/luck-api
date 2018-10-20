c0ff3a0b8cf8d1f3c902fe0f8c8a062f87a2a3a4
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-20 12:14:54 +0200

    updated MEMBER_INITIAL_LIST_MOCK

--- src/member/member.mock.ts
+++ src/member/member.mock.ts
@@ -3,45 +3,69 @@
 */
 export const MEMBER_INITIAL_LIST_MOCK = [
     {
-        id: '0',
-        name: 'Madden',
-        relationId: '20ae7a68-42e7-437f',
-        uniqueLink: '20ae7a68-42e7-437f-8d06-14a3b63746be',
-        matchedMemberId: null
+        'id': 0,
+        'name': 'Janet',
+        'uniqueLink': 'e2f28c6b-997c-4351-bdf7-613b1d548c3c',
+        'relationId': '53ae0bde-1c3c-4db7-90f7-14366243cdb9',
+        'eventMemberRole': {
+            'type': 'ORGANISER',
+            'matchedMemberId': null,
+            'abilityToRandom': false
+        }
     },
     {
-        id: '1',
-        name: 'Sloan',
-        relationId: 'fb453a3a-1373-4575',
-        uniqueLink: 'fb453a3a-1373-4575-9312-17d924383ea1',
-        matchedMemberId: null
+        'id': 1,
+        'name': 'Kline',
+        'uniqueLink': '57ddeb2a-7167-42ea-a0ff-658dde3e3da0',
+        'relationId': '8f1b628e-ce03-442d-b100-b45bb25dd6e6',
+        'eventMemberRole': {
+            'type': 'PARTICIPANT',
+            'matchedMemberId': null,
+            'abilityToRandom': true
+        }
     },
     {
-        id: '2',
-        name: 'Clemons',
-        relationId: '67664cb8-e500-460c',
-        uniqueLink: '67664cb8-e500-460c-b373-8c83aa185ae6',
-        matchedMemberId: null
+        'id': 2,
+        'name': 'Le',
+        'uniqueLink': 'd4f80eec-3e6c-4adb-bd66-3614b2267aee',
+        'relationId': '3b967f8a-159a-4b6c-be6e-793a86373137',
+        'eventMemberRole': {
+            'type': 'PARTICIPANT',
+            'matchedMemberId': null,
+            'abilityToRandom': true
+        }
     },
     {
-        id: '3',
-        name: 'Toni',
-        relationId: '7036177c-2c9f-47c7',
-        uniqueLink: '7036177c-2c9f-47c7-9ded-c30c3aa23271',
-        matchedMemberId: null
+        'id': 3,
+        'name': 'Barlow',
+        'uniqueLink': '1a4802c7-fc86-4e8f-9ad5-bb3caf490673',
+        'relationId': '45d3247e-42ce-4af9-b481-7f578fe7cb9f',
+        'eventMemberRole': {
+            'type': 'PARTICIPANT',
+            'matchedMemberId': null,
+            'abilityToRandom': true
+        }
     },
     {
-        id: '4',
-        name: 'Valeria',
-        relationId: '5711bba2-99b8-4da7',
-        uniqueLink: '5711bba2-99b8-4da7-a0c1-a567f6b724be',
-        matchedMemberId: null
+        'id': 4,
+        'name': 'Jean',
+        'uniqueLink': 'c7a9d059-0995-441d-ae21-5a9c9993d9f1',
+        'relationId': '0d0596af-90ff-4b96-8443-5663d775a79b',
+        'eventMemberRole': {
+            'type': 'PARTICIPANT',
+            'matchedMemberId': null,
+            'abilityToRandom': true
+        }
     },
     {
-        id: '5',
-        name: 'Elva',
-        relationId: 'ba575312-4713-41f9',
-        uniqueLink: 'ba575312-4713-41f9-a65e-b9858f2fe7cf',
-        matchedMemberId: null
+        'id': 5,
+        'name': 'Sheila',
+        'uniqueLink': 'a0a1522b-76d3-467d-9491-d16102216e10',
+        'relationId': 'baaa99d3-a6aa-41c6-8f5c-601cdea6b586',
+        'eventMemberRole': {
+            'type': 'PARTICIPANT',
+            'matchedMemberId': null,
+            'abilityToRandom': true
+        }
     }
-];
+]
