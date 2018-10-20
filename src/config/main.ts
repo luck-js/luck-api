1f008fc781df9ff0912113e6cc6db6f7f935673c
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-20 16:04:14 +0200

    created main config

---
+++ src/config/main.ts
@@ -0,0 +1,11 @@
+let main: {
+    port: number,
+    serverHost: string,
+    clientURL: string
+};
+
+export default main = {
+    port: 9001,
+    serverHost: 'http://localhost',
+    clientURL: 'http://localhost:9000'
+}
