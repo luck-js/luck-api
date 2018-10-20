6e1377f2947778ea3667608fad35522e22dc64cb
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-20 15:54:54 +0200

    moved test from participation to relation-member-happening.service

--- src/relation-member-happening/relation-member-happening.service.spec.ts
+++ src/relation-member-happening/relation-member-happening.service.spec.ts
@@ -5,6 +5,7 @@ import { initialDependencies } from '../test/test.spec';
 import { RelationMemberHappeningService } from './relation-member-happening.service';
 import { RelationMemberHappeningRepository } from './relation-member-happening.repository';
 import { PARTICIPANT_INITIAL_LIST_MOCK } from '../member/member.mock';
+import { MemberRepository } from '../member/member.repository';
 
 describe('Relation Member Happening Service', function () {
     let DIContainer: Container;
@@ -41,6 +42,23 @@ describe('Relation Member Happening Service', function () {
         })
     });
 
+    describe('Get data of member information view', function () {
+
+        it('Data should has correct member and happening name', function () {
+            const relationId = relationMemberHappeningService.createOwnerRelationOfHappening();
+            const MEMBER_NAME = 'Bill';
+            const HAPPENING_NAME = 'Happening Initial';
+
+            relationMemberHappeningService.editHappening(relationId, { name: HAPPENING_NAME });
+            const billParticipant = relationMemberHappeningService.addParticipant(relationId, MEMBER_NAME);
+
+            const memberInformationView = relationMemberHappeningService.getDataView(billParticipant.relationId);
+
+            assert.strictEqual(MEMBER_NAME, memberInformationView.member.name);
+            assert.strictEqual(HAPPENING_NAME, memberInformationView.happening.name);
+        });
+    });
+
     describe('Getter of IParticipantUniqueLinkData', function () {
         it('Returned list should be less by 1 member by ORGANISER', function () {
             const relationId = relationMemberHappeningService.createOwnerRelationOfHappening();
@@ -54,5 +72,29 @@ describe('Relation Member Happening Service', function () {
 
             assert.strictEqual(participantUniqueLinkData.length, happening.getMemberList().length - 1);
         })
+    });
+
+
+    describe('Get matched member', function () {
+
+        it('Should returned matched member if happening is published', function () {
+            const memberRepository: MemberRepository = DIContainer
+                .get<MemberRepository>(IDENTIFIER.MemberRepository);
+
+            const relationId = relationMemberHappeningService.createOwnerRelationOfHappening();
+            const relation = relationMemberHappeningRepository.get(relationId);
+            const happening = relation.getHappening();
+
+            const memberInstanceList = PARTICIPANT_INITIAL_LIST_MOCK
+                .map(({ name }) => relationMemberHappeningService.addParticipant(relationId, name));
+
+            let participant = memberInstanceList[0];
+
+            happening.publishEvent();
+            const matchedMember = relationMemberHappeningService.getMatchedMember(participant.relationId);
+            participant = memberRepository.getByIndex(participant.id);
+
+            assert.strictEqual(participant.eventMemberRole.MatchedMemberId, matchedMember.id);
+        });
     });
 });
