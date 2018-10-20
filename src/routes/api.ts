36cf51b23d6181a08e36ef0b12ea44d5e19207e6
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-20 16:19:33 +0200

    handled response for all method of API's

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -11,24 +11,24 @@ const participationHappeningApi = DIContainer.get<ParticipationHappeningApi>(IDE
 const happeningApi = DIContainer.get<HappeningApi>(IDENTIFIER.HappeningApi);
 
 router.route('/participation-happening/:id')
-    .get((req: Request, res: Response) => participationHappeningApi.getDataView(null));
+    .get((req: Request, res: Response) => participationHappeningApi.getDataView(req, res));
 
 router.route('/participation-happening/matched-member/:id')
-    .get((req: Request, res: Response) => participationHappeningApi.getMatchedMember(null));
+    .get((req: Request, res: Response) => participationHappeningApi.getMatchedMember(req, res));
 
 router.route('/happening/create')
-    .post((req: Request, res: Response) => happeningApi.create());
+    .post((req: Request, res: Response) => happeningApi.create(req, res));
 
 router.route('/happening/edit/:id')
-    .post((req: Request, res: Response) => happeningApi.edit(null, null));
+    .post((req: Request, res: Response) => happeningApi.edit(req, res));
 
 router.route('/happening/add-participant/:id')
-    .post((req: Request, res: Response) => happeningApi.addParticipant(null, null));
+    .post((req: Request, res: Response) => happeningApi.addParticipant(req, res));
 
 router.route('/happening/publish/:id')
-    .post((req: Request, res: Response) => happeningApi.publish(null));
+    .post((req: Request, res: Response) => happeningApi.publish(req, res));
 
 router.route('/happening/get-detailed-member-list-information/:id')
-    .get((req: Request, res: Response) => happeningApi.getDetailedParticipantListInformation(null));
+    .get((req: Request, res: Response) => happeningApi.getDetailedParticipantListInformation(req, res));
 
 export default router;
--- src/routes/participation-happening.api.ts
+++ src/routes/participation-happening.api.ts
@@ -1,13 +1,29 @@
+import { Request, Response } from 'express';
 import { RelationMemberHappeningService } from '../relation-member-happening/relation-member-happening.service';
 
 export class ParticipationHappeningApi {
-    constructor(private relationMemberHappeningService: RelationMemberHappeningService) {}
-
-    public getDataView(id: string) {
-        return this.relationMemberHappeningService.getDataView(id);
+    constructor(private relationMemberHappeningService: RelationMemberHappeningService) {
     }
 
-    public getMatchedMember(id: string) {
-        return this.relationMemberHappeningService.getMatchedMember(id);
+    public getDataView(req: Request, res: Response) {
+        try {
+            const { id } = req.params;
+            const memberInformationView = this.relationMemberHappeningService.getDataView(id);
+
+            res.send(memberInformationView);
+        } catch (err) {
+            res.send(err);
+        }
+    }
+
+    public getMatchedMember(req: Request, res: Response) {
+        try {
+            const { id } = req.params;
+            const memberView = this.relationMemberHappeningService.getMatchedMember(id);
+
+            res.send(memberView);
+        } catch (err) {
+            res.send(err);
+        }
     }
 }
