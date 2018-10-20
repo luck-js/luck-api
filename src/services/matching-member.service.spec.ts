f64d26466d666c08a4be6f00d3c4a50433468dba
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-20 12:53:33 +0200

    fixed test to more presented the truth

--- src/services/matching-member.service.spec.ts
+++ src/services/matching-member.service.spec.ts
@@ -1,22 +1,19 @@
 import * as assert from 'assert';
 import { Container } from 'inversify';
 import IDENTIFIER from '../identifiers';
-import { createHappening, initialDependencies } from '../test/test.spec';
+import { initialDependencies } from '../test/test.spec';
 import { MEMBER_INITIAL_LIST_MOCK } from '../member/member.mock';
-import { Happening } from '../happening/happening';
 import { MatchingMemberService } from './matching-member.service';
 import { MemberRepository } from '../member/member.repository';
 
 
 describe('Matching member', function () {
     let DIContainer: Container;
-    let happening: Happening;
     let memberRepository: MemberRepository;
     let newMemberList;
 
     before(function () {
         DIContainer = initialDependencies([...MEMBER_INITIAL_LIST_MOCK]);
-        happening = createHappening(DIContainer, { name: 'Initial Happening' });
         memberRepository = DIContainer.get<MemberRepository>(IDENTIFIER.MemberRepository);
 
         const matchingMemberService = DIContainer.get<MatchingMemberService>(IDENTIFIER.MatchingMemberService);
@@ -25,22 +22,22 @@ describe('Matching member', function () {
 
     it('Every member has random matched member', function () {
         newMemberList.forEach((member, index) => {
-            assert.strictEqual(true, typeof member.matchedMemberId === 'string')
+            assert.strictEqual(true, typeof member.MatchedMemberId === 'string')
         })
     });
 
     it('Matched member has another id', function () {
         newMemberList.forEach((member, index) => {
-            assert.strictEqual(true, member.id !== member.matchedMemberId)
+            assert.strictEqual(true, member.id !== member.MatchedMemberId)
         })
     });
 
     it('Every matched is unique', function () {
         newMemberList.reduce((previousState, member) => {
-            const isUnique = !previousState.some((matchedMemberId) => matchedMemberId === member.matchedMemberId);
+            const isUnique = !previousState.some((matchedMemberId) => matchedMemberId === member.MatchedMemberId);
             assert.strictEqual(true, isUnique);
             previousState.push(member.matchedMemberId);
             return previousState;
         }, [])
     });
-})
+});
