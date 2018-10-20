e310237b711c2a751e4588770817f5479f7087db
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-20 08:00:05 +0200

    implemented init value for event-member-role's

--- src/member/event-member-role/organiser/organiser.ts
+++ src/member/event-member-role/organiser/organiser.ts
@@ -2,6 +2,6 @@ import { EventMemberRole } from '../event-member-role';
 import { RoleType } from '../event-member-role.model';
 
 export class Organiser implements EventMemberRole {
-    type: RoleType;
+    type = RoleType.ORGANISER;
     matchedMemberId: string;
 }
