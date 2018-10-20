28a84c579d6f10e6f7e153f281eca9e912a40659
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-20 14:17:32 +0200

    refactored some private method to statics

--- src/services/matching-member.service.ts
+++ src/services/matching-member.service.ts
@@ -1,11 +1,22 @@
 import { injectable } from 'inversify';
 import { MatchedElement, MatchingService } from './matching.service';
-import { MemberRepository } from '../member/member.repository';
 import { Member } from '../member/member';
 
 
 @injectable()
 export class MatchingMemberService {
+
+    static filterMembersWhoAbleToRandom(memberList: Member[]): Member[] {
+        return memberList.filter((member) => member.eventMemberRole.abilityToRandom);
+    }
+
+    static mapMemberListToMatchingElementList(memberList: Member[]): MatchedElement[] {
+        return memberList.map((member) => {
+            const id = member.id;
+            const matchedId = member.MatchedMemberId;
+            return { id, matchedId }
+        });
+    }
 
     constructor(private matchingService: MatchingService) {
 
@@ -16,22 +27,10 @@ export class MatchingMemberService {
     }
 
     private matchMembersWhoAbleToRandom(memberList: Member[]): Member[] {
-        const participantList = this.filterMembersWhoAbleToRandom(memberList);
-        const matchedElementList = this.mapMemberListToMatchingElementList(participantList);
+        const participantList = MatchingMemberService.filterMembersWhoAbleToRandom(memberList);
+        const matchedElementList = MatchingMemberService.mapMemberListToMatchingElementList(participantList);
         const newMatchedElementList = this.matchingService.randomElements(matchedElementList);
         return this.mapMatchingElementListToElementList(memberList, newMatchedElementList);
-    }
-
-    private filterMembersWhoAbleToRandom(memberList: Member[]): Member[] {
-        return memberList.filter((member) => member.eventMemberRole.abilityToRandom);
-    }
-
-    private mapMemberListToMatchingElementList(memberList: Member[]): MatchedElement[] {
-        return memberList.map((member) => {
-            const id = member.id;
-            const matchedId = member.MatchedMemberId;
-            return { id, matchedId }
-        });
     }
 
     private mapMatchingElementListToElementList(memberList: Member[], matchedElementList: MatchedElement[]): Member[] {
