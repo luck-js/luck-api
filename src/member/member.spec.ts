f7933efef51fb3fa92a0e76563a8bedc0212f45f
Daniel Karski <daniel.karski5q@gmail.com>
2018-10-21 06:56:09 +0200

    removed Happening dependence from member test

--- src/member/member.spec.ts
+++ src/member/member.spec.ts
@@ -1,29 +1,26 @@
 import * as assert from 'assert';
 import { Container } from 'inversify';
 import { DIContainerProvider } from '../di-container';
-import { Happening } from '../happening/happening';
 import { MEMBER_INITIAL_LIST_MOCK } from './member.mock';
 import { RoleType } from './event-member-role/event-member-role.model';
-import { HappeningFactory } from '../happening/happening.factory';
+import { MemberFactory } from './member.factory';
 import IDENTIFIER from '../identifiers';
 
 
 describe('Member', function () {
     let DIContainer: Container;
-    let happeningFactory: HappeningFactory;
-    let happening: Happening;
+    let memberFactory: MemberFactory;
 
     beforeEach(function () {
         DIContainer = DIContainerProvider([...MEMBER_INITIAL_LIST_MOCK]);
-        happeningFactory = DIContainer.get<HappeningFactory>(IDENTIFIER.HappeningFactory);
-        happening = happeningFactory.create();
+        memberFactory = DIContainer.get<MemberFactory>(IDENTIFIER.MemberFactory);
     });
 
     describe('Creating new members', function () {
         it('Added member should be unique relationId ', function () {
-            const billMember = happening.addMember('a0a1522b-76d3-467d-9491-d16102216e10', RoleType.PARTICIPANT);
+            const billParticipant = memberFactory.create('a0a1522b-76d3-467d-9491-d16102216e10', RoleType.PARTICIPANT);
 
-            assert.notStrictEqual(billMember.relationId, null);
+            assert.notStrictEqual(billParticipant.relationId, null);
         });
     });
 });
