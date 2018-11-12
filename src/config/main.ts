73184f50f4bca474b30c7478a92aa52fa8963f65
maciejmatu <maciek.matuszewski@gmail.com>
2018-11-12 14:16:52 +0100

    Fix type-checking for configuration file

--- src/config/main.ts
+++ src/config/main.ts
@@ -1,18 +1,22 @@
 import * as dotenv from 'dotenv';
+import * as os from 'os';
+
 dotenv.config();
 
-let main: {
+interface Config {
     secret: string,
     database: string,
     port: string,
     serverHost: string,
-    clientURL: string
+    clientURL: string,
 };
 
-export default main = {
+const config: Config = {
     secret: '',
     database: process.env.DATABASE_URL,
     port: process.env.PORT,
-    serverHost: process.env.HOST,
+    serverHost: `http://${os.hostname()}`,
     clientURL: process.env.CLIENT_URL,
-}
+};
+
+export default config;
