1d907fb992ab48706f75c49fa712e7c48abe10bd
maciejmatu <maciek.matuszewski@gmail.com>
2018-11-12 14:16:31 +0100

    Simplify routes definitions

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -13,29 +13,37 @@ const DIContainer = DIContainerProvider([...MEMBER_INITIAL_LIST_MOCK], [...HAPPE
 const participationHappeningApi = DIContainer.get<ParticipationHappeningApi>(IDENTIFIER.ParticipationHappeningApi);
 const happeningApi = DIContainer.get<HappeningApi>(IDENTIFIER.HappeningApi);
 
-router.route('/participation-happening/:id')
-    .get((req: Request, res: Response) => participationHappeningApi.getDataView(req, res));
+router
+    .route('/participation-happening/:id')
+    .get(participationHappeningApi.getDataView);
 
-router.route('/participation-happening/matched-member/:id')
-    .get((req: Request, res: Response) => participationHappeningApi.getMatchedMember(req, res));
+router
+    .route('/participation-happening/matched-member/:id')
+    .get(participationHappeningApi.getMatchedMember);
 
-router.route('/happening/create')
-    .post((req: Request, res: Response) => happeningApi.create(req, res));
+router
+    .route('/happening/create')
+    .post(happeningApi.create);
 
-router.route('/happening/edit/:id')
-    .post((req: Request, res: Response) => happeningApi.edit(req, res));
+router
+    .route('/happening/edit/:id')
+    .post(happeningApi.edit);
 
-router.route('/happening/add-participant/:id')
-    .post((req: Request, res: Response) => happeningApi.addParticipant(req, res));
+router
+    .route('/happening/add-participant/:id')
+    .post(happeningApi.addParticipant);
 
-router.route('/happening/publish/:id')
-    .post((req: Request, res: Response) => happeningApi.publish(req, res));
+router
+    .route('/happening/publish/:id')
+    .post(happeningApi.publish);
 
-router.route('/happening/get-detailed-participant-list-information/:id')
-    .get((req: Request, res: Response) => happeningApi.getDetailedParticipantListInformation(req, res));
+router
+    .route('/happening/get-detailed-participant-list-information/:id')
+    .get(happeningApi.getDetailedParticipantListInformation);
 
-router.route('/happening/generate-detailed-participant-list-information/:id')
-    .get((req: Request, res: Response) => happeningApi.getGenerateDetailedParticipantListInformation(req, res))
-    .post((req: Request, res: Response) => happeningApi.generateDetailedParticipantListInformation(req, res));
+router
+    .route('/happening/generate-detailed-participant-list-information/:id')
+    .get(happeningApi.getGenerateDetailedParticipantListInformation)
+    .post(happeningApi.generateDetailedParticipantListInformation);
 
 export default router;
