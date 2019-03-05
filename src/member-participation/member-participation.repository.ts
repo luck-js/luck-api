bcec3a43c85acaee85856ca321e94e857bf168eb
Daniel Karski <daniel.karski5q@gmail.com>
2019-03-05 14:29:51 +0100

    removed memberParticipationFactory dependence from injected in repository

--- src/member-participation/member-participation.repository.ts
+++ src/member-participation/member-participation.repository.ts
@@ -1,24 +1,14 @@
 import { injectable } from 'inversify';
 import { Observable, from } from 'rxjs';
-import { map } from 'rxjs/operators';
 import MemberParticipationModel, { IMemberParticipation } from './member-participation.model';
-import { MemberParticipation } from './member-participation';
-import { MemberParticipationFactory } from './member-participation.factory';
 
 @injectable()
 export class MemberParticipationRepository {
-  constructor(
-    private list: IMemberParticipation[] = [],
-    private memberParticipationFactory: MemberParticipationFactory,
-  ) {}
-
   public add(memberParticipation: IMemberParticipation): Observable<IMemberParticipation> {
     return from(new MemberParticipationModel(memberParticipation).save());
   }
 
-  public getByIndex(id: string): Observable<MemberParticipation> {
-    return from(MemberParticipationModel.findOne({ id }, null, { limit: 1 }).exec()).pipe(
-      map(memberParticipation => this.memberParticipationFactory.recreate(memberParticipation)),
-    );
+  public getByIndex(id: string): Observable<IMemberParticipation> {
+    return from(MemberParticipationModel.findOne({ id }, null, { limit: 1 }).exec());
   }
 }
