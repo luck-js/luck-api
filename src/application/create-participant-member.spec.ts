6f81f805a5af64f4898a60375b15d0768a9ea570
Daniel Karski <daniel.karski5q@gmail.com>
2019-03-12 10:08:41 +0100

    Created create-participant-member test

---
+++ src/application/create-participant-member.spec.ts
@@ -0,0 +1,27 @@
+import 'reflect-metadata';
+import IDENTIFIER from '../infrastructure/identifiers';
+import { CreateMemberParticipation } from './create-member-participation';
+import * as assert from 'assert';
+import { ApplicationContainer } from './application.container';
+
+describe('CreateMemberParticipation', function() {
+  let createMemberParticipation: CreateMemberParticipation;
+
+  beforeEach(function() {
+    createMemberParticipation = ApplicationContainer.get<CreateMemberParticipation>(
+      IDENTIFIER.CreateMemberParticipation,
+    );
+  });
+
+  it('executed method return memberParticipation view value object', function() {
+    createMemberParticipation.execute().subscribe(memberParticipationView => {
+      assert.strictEqual(typeof memberParticipationView.happening.id, 'string');
+      assert.strictEqual(memberParticipationView.happening.description, '');
+      assert.strictEqual(memberParticipationView.happening.name, '');
+      assert.strictEqual(memberParticipationView.happening.isPublish, false);
+
+      assert.strictEqual(typeof memberParticipationView.member.id, 'string');
+      assert.strictEqual(memberParticipationView.member.name, '');
+    });
+  });
+});
